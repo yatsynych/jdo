6011c850ef150609ce90e37ecd332cb7d17f97ad
Volodymyr Yatsynych <yatsynych@gmail.com>
2022-01-31 17:06:21 +0200

    First commit

---
+++ jdo-front-end/src/todo/TodoItem.js
@@ -0,0 +1,25 @@
+import React, {useContext} from 'react';
+import Context from '../context';
+
+function TodoItem({todo,  index, onChange}) {
+    const {removeTodo} = useContext(Context)
+    const classes = []
+
+    if (todo.complited) {
+        classes.push('done');
+    }
+
+    return <li>
+        <span className={classes.join(' ')}>
+            <input
+                type="checkbox"
+                checked={todo.complited}
+                onChange={() => onChange(todo.id)}
+            />
+            {todo.title}
+        </span>
+        <button onClick={removeTodo.bind(null, todo.id)}>&times;</button>
+    </li>
+}
+
+export default TodoItem
